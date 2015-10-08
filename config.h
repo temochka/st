8ffdba074d7d06882870feab0fa2bea3959f0f8a
Artem Chistyakov <chistyakov.artem@gmail.com>
2015-10-08 14:17:32 -0400

    Scroll back with mouse wheel.

--- config.h
+++ config.h
@@ -128,14 +128,14 @@ static unsigned int defaultunderline = 7;
 /* Beware that overloading Button1 will disable the selection. */
 static Mousekey mkeys[] = {
 	/* button               mask            string */
-	{ Button4,              XK_NO_MOD,      "\031" },
-	{ Button5,              XK_NO_MOD,      "\005" },
+	{ Button4,              ShiftMask,      "\031" },
+	{ Button5,              ShiftMask,      "\005" },
 };
 
 static MouseShortcut mshortcuts[] = {
 	/* button               mask            function        argument */
-	{ Button4,              ShiftMask,      kscrollup,      { .i = 1 } },
-	{ Button5,              ShiftMask,      kscrolldown,    { .i = 1 } },
+	{ Button4,              XK_NO_MOD,      kscrollup,      { .i = 1 } },
+	{ Button5,              XK_NO_MOD,      kscrolldown,    { .i = 1 } },
 };
 
 /* Internal keyboard shortcuts. */
